4764f284809b19f11fee6e1ea20c5fc1b4ba8496
Lucas Starrett <grafixguy123@gmail.com>
2013-11-11 14:10:20 -0500

    add framework for mp3 handler functions to be used by the jukebox webserver

---
+++ python/mp3funcs.py
@@ -0,0 +1,22 @@
+# JUKEBOX mp3 interface
+#
+# Contains functions to handle play/pause, volume, manipulation, etc.
+# of mp3 files used in the Jukebox application
+#
+# AUTHORS: Lucas Starret, Jason Salter
+
+
+def main():
+	# Main won't be used in the finished project, use here for testing
+	testmp3func()
+
+
+# Sample mp3 library funciton
+def mp3func():
+	# TODO: implement mp3 opertaion. make more functions for other operations.
+	print "Hello, world!"
+
+
+# MAIN (call main automatically when 'python mp3funcs.py' is used from the terminal)
+if __name__ == '__main__':
+	main()
