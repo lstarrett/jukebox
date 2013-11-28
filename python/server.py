ef0624d4a854c2a8200b47d3bdc3e22c3589bcf9
Lucas Starrett <grafixguy123@gmail.com>
2013-11-27 21:25:12 -0500

    implemented server side upload (first pass, no file sanitation). Also implemented dynamic song list creation is JS

--- python/server.py
+++ python/server.py
@@ -29,7 +29,14 @@ class SYNC:
 
 class UPLOAD:
 	def POST(self):
-		print "UPLOAD request received, move mp3 to server"
+		uploaded = web.input(mp3file={})
+#		web.debug(uploaded['mp3file'].filename) # This is the filename
+#		web.debug(uploaded['mp3file'].value) # This is the file contents
+#		web.debug(uploaded['mp3file'].file.read()) # Or use a file(-like) object
+#		data.saveFile(uploaded['mp3file'].filename, uploaded['mp3file'].file)
+		with open(uploaded['mp3file'].filename, 'w') as music:
+		    music.write(uploaded['mp3file'].value)
+		raise web.seeother('/')
 
 
 if __name__ == '__main__':
--- static/js/jukebox.js
+++ static/js/jukebox.js
@@ -19,6 +19,59 @@ $(function() {
 	});
 });
 
+
+// Shift list items down when a new one is added
+function prependListItem(listName, listItemHTML){
+	$("#placeholders li:first-child")
+		.slideUp('fast', function() { $(this).remove() })
+	$(listItemHTML)
+		.hide()
+		.css('opacity',0.0)
+		.prependTo('#' + listName)
+		.slideDown('fast')
+		.animate({opacity: 1.0})
+}
+
+
+// Display file chooser when upload button is clicked
 $("#upload").click(function() {
-	$("#chooser").click();
+	//$("#chooser").click();
+	// add an item to the list, just to see it work
+	var item = document.createElement("li");
+	item.className = 'card song-block';
+	item.innerHTML = 'Song 6<div class="card song-remove-btn">X</div>'
+	prependListItem('sortable', item);
+	$('#sortable').sortable();
 });
+
+// When a file is chosen, push it to the server
+$("#chooser").change(function() {
+	$("#chooser-submit").click();
+
+
+
+	/*alert("chooser has a file!");
+	//var file = this.files[0];
+	//var formData = new FormData($("#fileform")[0]);
+	var formData = new FormData($('form')[0]);
+	$.ajax({
+		url: 'upload',
+		type: 'POST',
+		xhr: function() {  // Custom XMLHttpRequest
+			var myXhr = $.ajaxSettings.xhr();
+			if(myXhr.upload){ // Check if upload property exists
+				myXhr.upload.addEventListener('progress',progressHandlingFunction, false); // For handling the progress of the upload
+			}
+			return myXhr;
+		},
+		success: null,
+		error: function(xhr, status) {
+			alert(xhr.status)
+		},
+		data: formData,
+		cache: false,
+		contentType: false,
+		processData: false,
+	});*/
+});
+
