2e0205265efaabfccc541b264b6936604dac6c02
Lucas Starrett <grafixguy123@gmail.com>
2013-12-01 14:01:31 -0500

    implemented user login functionality and keep-alives to prevent lingering participating users.

--- python/server.py
+++ python/server.py
@@ -5,12 +5,16 @@ urls = (
 	'/', 'INDEX',
 	'/(css|js|fonts)/(.*)', 'SERVE_STATIC',
 	'/sync', 'SYNC',
-	'/upload', 'UPLOAD'
+	'/upload', 'UPLOAD',
+	'/join', 'JOIN'
 )
 
 # State which is synced between clients
-state = { 'controlling':'foo', 'users':["Bob","Rutherford","Shawntelle","Keith"], 'songs':["Song 1", "Song 40", "Song Poop"] }
+#state = { 'controlling':'none', 'users':["Bob","Rutherford","Shawntelle","Keith"], 'songs':["Song 1", "Song 40", "Song Poop"] }
+state = { 'controlling':'none', 'users':[], 'songs':[] }
 
+# State which is synced between clients
+keep_alives = {}
 
 # serve index.html
 class INDEX:
@@ -26,12 +30,39 @@ class SERVE_STATIC:
 
 class SYNC:
 	def GET(self):
+		global state
 		print "SYNC GET reqeust received, send state data to client"
+
+		# extract client request data and process it
+		key, value = web.input().popitem()
+		if (key == 'none'): # continue, this is just a spectator
+			print "DEBUG: 'none' received, this is a spectator"
+		elif (key in state['users']):
+			print "DEBUG: this is a known user, NOT controlling. Resetting keep-alive"
+			keep_alives[key] = 100
+		elif ('controlling' in key): # simple sanity check for good JSON data
+			print "DEBUG: this is a known user, CONTROLLING. Syncing state data with user and resetting keep-alive"
+			state = json.loads(key)
+			keep_alives[state['controlling']] = 100
+		elif (key.isalnum() and len(key) < 15): # check for sanitized data and add to users
+			print "DEBUG: this is an unknown user. Adding to users and setting keep-alive"
+			state['users'].append(key)
+			keep_alives[key] = 100
+
+		# subtract some "time" from each user's keepalive value. Note that this is NOT real time, and "time" only
+		# decreases when at least one user is online syncing with the server.
+		print "================KEEP ALIVE DIAGNOSTIC================="
+		keep_alives.update((user, time - 20) for user, time in keep_alives.items())
+		for user, time in keep_alives.items():
+			print "user: " + user + ", time: " + str(time)
+			if (time <= 0):
+				state['users'].remove(user)
+				del keep_alives[user]
+		print "================KEEP ALIVE DIAGNOSTIC================="
+
+		# return the current state information
 		web.header('Content-Type', 'application/json')
 		return json.dumps(state)
-	
-	def POST(self):
-		print "SYNC POST reqeust received, update server state data"
 
 class UPLOAD:
 	def POST(self):
--- static/js/jukebox.js
+++ static/js/jukebox.js
@@ -1,141 +1,193 @@
-// Object that contains all the state information of the jukebox
-// which is distributed to all clients
-var state;
-
-setInterval(function() {
+// Don't run scripts until initial document is loaded
+$(document).ready(function() {
+	
+	// Object that contains all the state information of the jukebox
+	// which is distributed to all clients
+	var state = null;
+	var me = 'none';
+	var controlling = false;
+	
+	// Intitial sync with server, then routine update polling
 	$.getJSON('sync', function(data) {
 		state = data;
-		console.log(JSON.stringify(state));
-		console.log(state.controlling);
+		console.log(JSON.stringify(state));//DEBUG
 	});
-	setupSongs();
-	setupUsers();
-}, 2000);
-
-// Setup songs
-function setupSongs() {
-	$('#sortable').empty();
-	$('#placeholders').empty();
-
-	for (var i = 0; i < 6; i++) {
+	setInterval(function() {
+		var control = me;
+		if (state != null && controlling == true) {
+			control = JSON.stringify(state);
+			console.log("DEBUG: apparently this guy is in control");//DEBUG
+		}
+		$.getJSON('sync', control, function(data) {
+			state = data;
+		});
+		if (!controlling) {
+			setupSongs();
+			setupUsers();
+		}
+	}, 5000);
+	
+	// Setup songs
+	function setupSongs() {
+		$('#sortable').empty();
+		$('#placeholders').empty();
+	
+		for (var i = 0; i < 6; i++) {
+			var placeholder = document.createElement("li");
+			placeholder.className = 'card song-block';
+			placeholder.style.visibility = 'hidden';
+			$(placeholder).prependTo('#placeholders');
+		}
+	
+		for (var i = 0; i < state.songs.length; i++) {
+			$('#placeholders li:first-child').remove();
+			var song = document.createElement("li");
+			song.className = 'card song-block';
+			song.innerHTML = state.songs[i] + '<div class="card song-remove-btn">X</div>';
+			$(song).prependTo('#sortable');
+		}
+	}
+	
+	// Setup users
+	function setupUsers() {
+		$('#userlist').empty();
+		for (var i = 0; i < state.users.length; i++) {
+			var user = document.createElement("li");
+			user.className = 'card userlist-user';
+			user.innerHTML = state.users[i];
+			$(user).prependTo('#userlist');
+		}
+	}
+	
+	
+	// Make songs rearrangeable
+	$(function() {
+		$('#sortable').sortable();
+	});
+	
+	// Add new song
+	function addSong(listItem){
+		// Add song to state
+		state.songs.push(listItem.id);
+		console.log(JSON.stringify(state));//DEBUG
+		// Push song onto HTML list
+		$("#placeholders li:first-child")
+			.remove()
+		$(listItem)
+			.css('opacity',0.0)
+			.prependTo('#sortable')
+			.animate({opacity: 1.0})
+	}
+	
+	// Remove a song
+	function removeSong(listItem){
+		// Remove song from state
+		state.songs.splice(state.songs.indexOf(listItem.id), 1);
+		console.log(JSON.stringify(state));//DEBUG
+	
 		var placeholder = document.createElement("li");
 		placeholder.className = 'card song-block';
 		placeholder.style.visibility = 'hidden';
-		$(placeholder).prependTo('#placeholders');
+		$(listItem)
+			.animate({opacity: 0.0}, function() {
+				$(listItem)
+					.slideUp('fast', function() {
+						this.remove()
+					})
+				$(placeholder)
+					.hide()
+					.prependTo('#placeholders')
+					.slideDown('fast')
+			})
 	}
-
-	for (var i = 0; i < state.songs.length; i++) {
-		$('#placeholders li:first-child').remove();
-		var song = document.createElement("li");
-		song.className = 'card song-block';
-		song.innerHTML = state.songs[i] + '<div class="card song-remove-btn" id="foo">X</div>';
-		$(song).prependTo('#sortable');
-	}
-}
-
-// Setup users
-function setupUsers() {
-	$('#userlist').empty();
-	for (var i = 0; i < state.users.length; i++) {
-		var user = document.createElement("li");
-		user.className = 'card userlist-user';
-		user.innerHTML = state.users[i];
-		$(user).prependTo('#userlist');
-	}
-}
-
-
-// Make songs rearrangeable
-$(function() {
-	$('#sortable').sortable();
-});
-
-// Add new song
-function addSong(listItem){
-	$("#placeholders li:first-child")
-		.remove()
-	$(listItem)
-		.css('opacity',0.0)
-		.prependTo('#sortable')
-		.animate({opacity: 1.0})
-}
-
-// Remove a song
-function removeSong(listItem){
-	var placeholder = document.createElement("li");
-	placeholder.className = 'card song-block';
-	placeholder.style.visibility = 'hidden';
-	$(listItem)
-		.animate({opacity: 0.0}, function() {
-			$(listItem)
-				.slideUp('fast', function() {
-					this.remove()
-				})
-			$(placeholder)
-				.hide()
-				.prependTo('#placeholders')
-				.slideDown('fast')
-		})
-}
-
-// Display file chooser when upload button is clicked
-$(document).on('click', '.song-remove-btn', function(){
-	removeSong($(this).parent());
-});
-
-// Display file chooser when upload button is clicked
-$("#upload").click(function() {
-	//$("#chooser").click();
-	// add an item to the list, just to see it work
-	var item = document.createElement("li");
-	item.className = 'card song-block';
-	item.innerHTML = 'Song 6<div class="card song-remove-btn" id="foo">X</div>'
-	addSong(item);
-	$('#sortable').sortable();
-});
-
-// When a file is chosen, push it to the server
-$("#chooser").change(function() {
-	$("#chooser-submit").click();
-
-	// TODO: I wish uploads worked over ajax. It would be cleaner.
-	/*alert("chooser has a file!");
-	//var file = this.files[0];
-	//var formData = new FormData($("#fileform")[0]);
-	var formData = new FormData($('form')[0]);
-	$.ajax({
-		url: 'upload',
-		type: 'POST',
-		xhr: function() {  // Custom XMLHttpRequest
-			var myXhr = $.ajaxSettings.xhr();
-			if(myXhr.upload){ // Check if upload property exists
-				myXhr.upload.addEventListener('progress',progressHandlingFunction, false); // For handling the progress of the upload
+	
+	// Display file chooser when upload button is clicked
+	$(document).on('click', '.song-remove-btn', function(){
+		removeSong($(this).parent());
+	});
+	
+	// Display file chooser when upload button is clicked
+	$("#upload").click(function() {
+		//$("#chooser").click();
+		// add an item to the list, just to see it work
+		var item = document.createElement("li");
+		item.id = 'Uploaded Song';
+		item.className = 'card song-block';
+		item.innerHTML = item.id + '<div class="card song-remove-btn">X</div>'
+		addSong(item);
+		$('#sortable').sortable();
+	});
+	
+	// When a file is chosen, push it to the server
+	$("#chooser").change(function() {
+		$("#chooser-submit").click();
+	
+		// TODO: I wish uploads worked over ajax. It would be cleaner.
+		/*alert("chooser has a file!");
+		//var file = this.files[0];
+		//var formData = new FormData($("#fileform")[0]);
+		var formData = new FormData($('form')[0]);
+		$.ajax({
+			url: 'upload',
+			type: 'POST',
+			xhr: function() {  // Custom XMLHttpRequest
+				var myXhr = $.ajaxSettings.xhr();
+				if(myXhr.upload){ // Check if upload property exists
+					myXhr.upload.addEventListener('progress',progressHandlingFunction, false); // For handling the progress of the upload
+				}
+				return myXhr;
+			},
+			success: null,
+			error: function(xhr, status) {
+				alert(xhr.status)
+			},
+			data: formData,
+			cache: false,
+			contentType: false,
+			processData: false,
+		});*/
+	});
+	
+	// Participate dialog
+	$(function() {
+		$('#participate').avgrund({
+			height: 230,
+			holderClass: 'card',
+			template: '    <div class="card welcome-modal">WELCOME!</div>' +
+			          '    <div class="form-group">' +
+			          '        <input type="text" value placeholder="Nickname" id="nickname" class="form-control">' +
+			          '    </div>' +
+			          '    <div class="form-group has-success">' +
+			          '        <input type="text" value placeholder="Channel Password" id="password" class="form-control">' +
+			          '        <span class="input-icon fui-check-inverted"></span>' +
+			          '    </div>'
+		});
+		
+		// when the password box is in focus, continuously check password
+		var checkloop;
+		$(document).on('focus', '#password', function(){
+			checkloop = setInterval(function() {
+				console.log("Checking password...");
+				var nickname = $('#nickname').val();
+				var password = $('#password').val();
+				if (nickname.length > 0 && valid(nickname) && password == 'pimpsonly'){ //TODO: replace plaintext check with hash
+					clearInterval(checkloop);
+					// Add user and change "participate" to "take control"
+					console.log("Welcome!");//DEBUG
+					me = nickname;
+				}
+			}, 100); // check 10x per second
+		});
+		$(document).on('focusout', '#password', function(){
+			clearInterval(checkloop);
+		});
+		// make sure nickname is alphanumeric
+		function valid(nickname) {
+			var regex = new RegExp("^[a-zA-Z0-9]+$");
+			if (regex.test(nickname)) {
+				return true;
 			}
-			return myXhr;
-		},
-		success: null,
-		error: function(xhr, status) {
-			alert(xhr.status)
-		},
-		data: formData,
-		cache: false,
-		contentType: false,
-		processData: false,
-	});*/
-});
-
-// Display modal dialog using Avgrund
-$(function() {
-	$('#participate').avgrund({
-		height: 230,
-		holderClass: 'card',
-		template: '    <div class="card welcome-modal">WELCOME!</div>' +
-		          '    <div class="form-group">' +
-		          '        <input type="text" value placeholder="Nickname" class="form-control">' +
-		          '    </div>' +
-		          '    <div class="form-group has-success">' +
-		          '        <input type="text" value placeholder="Channel Password" class="form-control">' +
-		          '        <span class="input-icon fui-check-inverted"></span>' +
-		          '    </div>'
+			return false;
+		}
 	});
 });
